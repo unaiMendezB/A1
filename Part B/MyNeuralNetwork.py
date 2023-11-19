2f3d43fa3a8ae125a72e45b5a21c1721f18435f6
unaimendez <unai.mendez@estudiants.urv.cat>
2023-11-19 16:19:38 +0100

    no va

--- Part B/MyNeuralNetwork.py
+++ Part B/MyNeuralNetwork.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 
-
 # Neural Network class
 class MyNeuralNetwork:
     def __init__(self, layers, epochs, learning_rate, momentum, activation_function, validation_set_percentage):
@@ -19,43 +18,35 @@ class MyNeuralNetwork:
         self.z = []
 
         self.w = []  # Weights for each layer
-        self.w.append(np.zeros((1, 1)))
 
         self.theta = []  # Thresholds for each layer
-        self.theta.append(np.zeros((1, 1)))
 
         self.delta = []  # Error propagation for each layer
-        self.delta.append(np.zeros((1, 1)))
 
         self.d_w = []  # Weight changes for each layer
-        self.d_w.append(np.zeros((1, 1)))
 
         self.d_theta = []  # Threshold changes for each layer
-        self.d_theta.append(np.zeros((1, 1)))
 
         self.d_w_prev = []  # Previous weight changes for momentum calculation
-        self.d_w_prev.append(np.zeros((1, 1)))
 
         self.d_theta_prev = []  # Previous threshold changes for momentum calculation
-        self.d_theta_prev.append(np.zeros((1, 1)))
 
         self.train_errors = []  # Training errors for each epoch
         self.val_errors = []  # Validation errors for each epoch
 
         for lay in range(self.L):
             self.xi.append(np.zeros(layers[lay]))
-
-        for lay in range(1, self.L):
             self.w.append(np.zeros((layers[lay], layers[lay - 1])))
+            self.z.append(np.zeros((layers[lay], layers[lay - 1])))
             self.theta.append(np.zeros(layers[lay]))
             self.delta.append(np.zeros(layers[lay]))
             self.d_w.append(np.zeros((layers[lay], layers[lay - 1])))
             self.d_theta.append(np.zeros(layers[lay]))
             self.d_w_prev.append(np.zeros((layers[lay], layers[lay - 1])))
             self.d_theta_prev.append(np.zeros(layers[lay]))
-        # Separates the data to train from the one to Test
 
-    def split_dataset(self,X, y):
+    # Separates the data to train from the one to Test
+    def split_dataset(self, X, y):
         num_test = int(self.validation_set_percentage * len(y))
 
         # Shuffle the data (for turb or synt we dont need this but it can be helpfull for other datasets)
@@ -73,22 +64,22 @@ class MyNeuralNetwork:
     def activate(self, x):
         if self.fact == 'sigmoid':
             return 1 / (1 + np.exp(-x))
-        elif self.fact  == 'relu':
+        elif self.fact == 'relu':
             return np.maximum(0, x)
-        elif self.fact  == 'linear':
+        elif self.fact == 'linear':
             return x
-        elif self.fact  == 'tanh':
+        elif self.fact == 'tanh':
             return np.tanh(x)
 
     # Switch for activation_derivate_function
     def activate_derivative(self, x):
-        if self.fact  == 'sigmoid':
+        if self.fact == 'sigmoid':
             return x * (1 - x)
-        elif self.fact  == 'relu':
+        elif self.fact == 'relu':
             return np.where(x > 0, 1, 0)
-        elif self.fact  == 'linear':
+        elif self.fact == 'linear':
             return np.ones_like(x)
-        elif self.fact  == 'tanh':
+        elif self.fact == 'tanh':
             return 1 - np.tanh(x) ** 2
 
     # This method allows us to train the network with this data.
@@ -97,8 +88,8 @@ class MyNeuralNetwork:
     # y of size (n_samples), which holds the target values (class labels) for the training samples.
     def fit(self, X, y):
         X_train, X_val, y_train, y_val = self.split_dataset(X, y)
-        for epoch in range(self.epochs):    #  (3)
-            for pat in range(X.shape[0]):   #  (4)
+        for epoch in range(self.epochs):  # (3)
+            for pat in range(X.shape[0]):  # (4)
                 # Choose a random pattern (5)
                 x = X[pat]
                 z = y[pat]
@@ -112,7 +103,9 @@ class MyNeuralNetwork:
                 # Back-propagate the error (7)
                 self.delta[-1] = (self.xi[-1] - z) * self.activate_derivative(self.xi[-1])
                 for lay in range(self.L - 2, 0, -1):
-                    self.delta[lay] = np.dot(self.w[lay + 1].T, self.delta[lay + 1]) * self.activate_derivative(self.xi[lay])
+                    self.delta[lay] = np.dot(self.w[lay + 1].T, self.delta[lay + 1]) * self.activate_derivative(
+                        self.xi[lay])
+
                 # Update the weights and thresholds (8)
                 for lay in range(1, self.L):
                     self.d_w[lay] = np.dot(self.delta[lay], self.xi[lay - 1].T)
@@ -147,7 +140,7 @@ class MyNeuralNetwork:
             self.xi[0] = x
             for lay in range(1, self.L):
                 self.z[lay] = np.dot(self.w[lay], self.xi[lay - 1]) + self.theta[lay]
-                self.xi[lay] =  self.activate(self.z[lay])
+                self.xi[lay] = self.activate(self.z[lay])
 
             # The output of the network is the output of the last layer
             predictions.append(self.xi[-1])
@@ -159,35 +152,15 @@ class MyNeuralNetwork:
     def loss_epochs(self):
         return np.array(self.train_errors), np.array(self.val_errors)
 
-'''
-1 Scale input and/or output patterns, if needed
-2 Initialize all weights and thresholds randomly
-3 For epoch = 1 To num epochs
-4   For pat = 1 To num training patterns
-5     Choose a random pattern (x, z) of the training set
-6     Feed􀀀forward propagation of pattern x to obtain the output o(x)
-7     Back􀀀propagate the error for this pattern
-8     Update the weights and thresholds
-9   End For
-10  Feed􀀀forward all training patterns and calculate their prediction quadratic error
-11  Feed􀀀forward all validation patterns and calculate their prediction quadratic error
-12 End For
-13 # Optional: Plot the evolution of the training and validation errors
-14 Feed􀀀forward all test patterns
-15 Descale the predictions of test patterns, and evaluate them
-'''
 
-'''
-dataTurb = pd.read_csv('turbine_Standardized.txt', sep='\t')
-'''
+# dataTurb = pd.read_csv('turbine_Standardized.txt', sep='\t')
 dataSynth = pd.read_csv('synthetic_Normalized.txt', sep='\t')
 X = dataSynth.iloc[:, :-1].values  # Create the array X
-y = dataSynth.iloc[:, -1].values # Create the vector y
+y = dataSynth.iloc[:, -1].values  # Create the vector y
 
 layers = [4, 9, 5, 1]  # layers include input layer + hidden layers + output layer
 
 nn = MyNeuralNetwork(layers, 1000, 0.01, 0.0, 'sigmoid', 0.2)  # Creation nn
-
 nn.fit(X, y)  # Training
 train_errors, val_errors = nn.loss_epochs()
 
